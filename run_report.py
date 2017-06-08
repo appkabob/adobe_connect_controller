157a1e8c980f8de9e9e5770948ffa383c91e22bb
Nick Alexander <nick@appkabob.com>
2017-06-08 17:27:12 -0500

    Removed project-specific files

--- run_report.py
+++
@@ -1,9 +0,0 @@
-from models.connect import Connect
-from models.meeting import Meeting
-
-
-Connect()
-meeting = Meeting('Live Chat 3', '2017-03-13', '67877468')
-attendees = meeting.get_attendees()
-for attendee in attendees:
-    print(attendee)
